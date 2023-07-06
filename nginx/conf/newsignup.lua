90370b7d39fbcd7b8e6bb0fac48c91851f6b731b
wilu2 <75051706+wilu2@users.noreply.github.com>
2023-07-06 17:35:44 +0800

    Update newsignup.lua

--- nginx/conf/newsignup.lua
+++ nginx/conf/newsignup.lua
@@ -2,28 +2,23 @@ local mysql = require "resty.mysql"
 local json = require "cjson"                                                                                                                                                                               
                                                                                                                                                                                                            
 local function create_mysql_connection()                                                                                                                                                                   
-    local db, err = mysql:new()                                                                                                                                                                            
-    if not db then                                                                                                                                                                                         
-        ngx.log(ngx.ERR,"failed to create mysql connextion:",err)                                                                                                                                          
-        return nil,err                                                                                                                                                                                     
-    end                                                                                                                                                                                                    
-    local ok, err = db:connect{                                                                                                                                                                            
-        host = "10.0.2.15",                                                                                                                                                                                
-        port = 13306,                                                                                                                                                                                      
-        database = "app",                                                                                                                                                                                  
-        user = "root",                                                                                                                                                                                     
-        password = "1234567",                                                                                                                                                                              
-        charset = "utf8mb4",                                                                                                                                                                               
-    }                                                                                                                                                                                                      
-                                                                                                                                                                                                           
-    if not ok then                                                                                                                                                                                         
-        ngx.say("connect mysql failed:", err)                                                                                                                                                              
-        db:close()                                                                                                                                                                                         
-        return nil,err                                                                                                                                                                                     
-    end                                                                                                                                                                                                    
-    return db                                                                                                                                                                                              
-end                                                                                                                                                                                                        
-                                                                                                                                                                                                           
+    local mysql_config = globe_config.mysql
+    local db, err = mysql:new()
+    if not db then
+        ngx.log(ngx.ERR, "Failed to create MySQL connection: ", err)
+        return nil, err
+    end
+    local ok, err = db:connect(mysql_config)
+
+    if not ok then
+        ngx.say("Failed to connect to MySQL: ", err)
+        db:close()
+        return nil, err
+    end
+
+    return db
+end                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                         
 local function signup_handler()                                                                                                                                                                            
                                                                                                                                                                                                            
     ngx.req.read_body()                                                                                                                                                                                    
@@ -44,9 +39,7 @@ local function signup_handler()
         ngx.say(json.encode({code = 1, msg = "Passwords do not match"}))                                                                                                                                   
         return                                                                                                                                                                                             
     end                                                                                                                                                                                                   
-     ngx.say(json.encode({code = 1, msg = username}))   
-		ngx.say(json.encode({code = 1, msg = password}))   
-		ngx.say(json.encode({code = 1, msg = repassword}))   
+        
 
      local db, err = create_mysql_connection()                                                                                                                                                             
      if not db then                                                                                                                                                                                        
@@ -70,10 +63,7 @@ local function signup_handler()
         ngx.say(json.encode({code = 1, msg = "user already exist"}))
         return
     end
-    sql = [[insert into user (user_id, username, password) values ( ]]..userid.. [[,"]] ..username..[[",]]..password..[[)]]                                                                                                                                                                                                       
-    --local sql = "insert into user (user_id, username, password) values ("..userid..","..ngx.quote_sql_str(username)..","..ngx.quote_sql_str(password)..")"                                               
-    ngx.say(json.encode({code = 1, msg = sql}))                                                                                                                                                                                                        
-    --local sql = [[insert into user (user_id, username, password) values (23456,"ananimal","1234567")]]                                                                                                     
+    sql = [[insert into user (user_id, username, password) values ( ]]..userid.. [[,"]] ..username..[[",]]..password..[[)]]                                                                                                                                                                                                                                                                                                          
     local res, err = db:query(sql)                                                                                                                                                             
     if not res then                                                                                                                                                                                        
         ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR                                                                                                                                                        
@@ -92,4 +82,4 @@ if ngx.req.get_method() == "POST" and uri_args and uri_args.action == "signup" t
 else                                                                                                                                                                                                       
     ngx.status = ngx.HTTP_NOT_FOUND                                                                                                                                                                        
     ngx.say("page not found")                                                                                                                                                                              
-end                                    
+end                           
