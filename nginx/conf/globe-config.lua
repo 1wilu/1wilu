38481c71fdba7952e9ec427d09f97ae5e57032e0
wilu2 <75051706+wilu2@users.noreply.github.com>
2023-07-06 17:36:37 +0800

    Create globe-config.lua

---
+++ nginx/conf/globe-config.lua
@@ -0,0 +1,11 @@
+
+globe_config = {
+	mysql = {
+	host = "10.0.2.15",
+        port = 13306,
+        database = "app",
+        user = "root",
+        password = "1234567",
+        charset = "utf8mb4",
+	}
+}
